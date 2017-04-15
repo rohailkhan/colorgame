0afd7c16c20d247619ccc8d8d786314c1248be7d
Rohl Khan <rohailkhan@gmail.com>
2017-04-15 23:10:42 +1200

    added alerts.If right color clicked it tells that thats right

--- colorGame.js
+++ colorGame.js
@@ -1,13 +1,31 @@
 var rgb=[
-"rgb(255 , 0, 0)" ,
-"rgb(0 , 255, 0)" ,
-"rgb(0 , 0, 255)" ,
-"rgb(255 , 255, 0)" ,
-"rgb(255 , 0, 255)" ,
-"rgb(0 , 255, 255)" ]
+"rgb(255, 0, 0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)" ]
 
 var squares=document.querySelectorAll("#square");
+var pickedcolor=rgb[0];
+var message=document.querySelector("#message");
 
 for(var i=0;i<squares.length;i++){
+	//add colors to squares
 	squares[i].style.background=rgb[i];
+	//add event listeners
+	squares[i].addEventListener("click" , function(){
+		//grab color of the click
+		var clickedcolor=this.style.background;
+		if(clickedcolor===pickedcolor){
+			console.log("right color")
+			alert("thats right"); }
+		else 
+			{alert("try again");
+		console.log("wrong color")}
+	});
 }
+
+var colordisplay=document.getElementById("colordisplay");
+colordisplay.textContent=pickedcolor;
+
