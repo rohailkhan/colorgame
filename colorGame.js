572c95114542b651032b660e1b31a28b641199de
Rohl Khan <rohailkhan@gmail.com>
2017-04-16 17:43:37 +1200

    with the right color click,all the headings also become equal to the clicked color

--- colorGame.js
+++ colorGame.js
@@ -9,7 +9,7 @@ var rgb=[
 var squares=document.querySelectorAll("#square");
 var pickedcolor=rgb[0];
 var message=document.querySelector("#message");
-
+var h1=document.querySelector("#headingdiv");
 for(var i=0;i<squares.length;i++){
 	//add colors to squares
 	squares[i].style.background=rgb[i];
@@ -19,15 +19,23 @@ for(var i=0;i<squares.length;i++){
 		var clickedcolor=this.style.background;
 		if(clickedcolor===pickedcolor){
 			console.log("right color")
-			message.textContent="thats right"; }
+			message.textContent="thats right"; 
+			allsame();
+			h1.style.background=clickedcolor;}
 		else 
-			{this.style.background="grey";}
+			{this.style.background="grey"; message.textContent="thats wronggggggg";}
 	});
 }
 
 var colordisplay=document.getElementById("colordisplay");
 colordisplay.textContent=pickedcolor;
 
+function allsame(){
+	for (var i=0;i<squares.length;i++){
+		squares[i].style.background=pickedcolor;
+
+	}
+}
 // function randomcolors(){
 // 	arr=[];
 // 	arr.push(random);
